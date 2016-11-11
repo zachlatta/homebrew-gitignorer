3fe0cb20e77c0c93814f50325c10a157e6f59584
Dennis Rippinger <dennis.rippinger@gmail.com>
2016-11-11 14:16:43 +0100

    Updated Version 1.0.1 and hash to SHA256 * With the newest version of homebrew sha1 compatibility was disabled * Added a small description * Small changes to make the linter happy

--- gitignorer.rb
+++ gitignorer.rb
@@ -1,9 +1,8 @@
-require 'formula'
-
 class Gitignorer < Formula
+  desc 'Easily create .gitignore files for your projects'
   homepage 'https://github.com/zachlatta/gitignorer'
-  url 'https://github.com/zachlatta/gitignorer/archive/v1.0.0.tar.gz'
-  sha1 'bb2726f86a7054cde3fc2713a3fecd8bd0334fed'
+  url 'https://github.com/zachlatta/gitignorer/archive/1.0.1.tar.gz'
+  sha256 '220c13b4359d74255ae2ed7d8ac8d8e4372aae5be41682ff50df3a0ecea91667'
 
   depends_on 'go' => :build
 
@@ -11,11 +10,11 @@ class Gitignorer < Formula
     ENV['GOPATH'] = buildpath
 
     directory = "github.com/zachlatta/gitignorer"
-    system "mkdir -p src/#{ directory }"
-    system "mv `ls -A | grep -v src` ./src/#{ directory }"
-    system "cd src/#{ directory }; go get"
+    system "mkdir -p src/#{directory}"
+    system "mv `ls -A | grep -v src` ./src/#{directory}"
+    system "cd src/#{directory}; go get"
 
-    system "cd src/#{ directory }; go install"
+    system "cd src/#{directory}; go install"
     bin.install 'bin/gitignorer'
   end
 
